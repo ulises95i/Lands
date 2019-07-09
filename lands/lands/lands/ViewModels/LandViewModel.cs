a33bf60be7569052fed48d62c448e4edadd0d4ba
ulises95i <52174712+ulises95i@users.noreply.github.com>
2019-07-09 11:01:22 -0500

    Mostrar una lista en la tabpage curencies y Bordes
    Se recorre la lista Land para poder obtener la collecion bordes y pararla aun obeservablecollections...
    
    Currencies ya no es necesario recorrer la lista por que ya tenemos un lista queviene del servicio y  solo basta con convertir la lista en un obsevablecollection

--- lands/lands/lands/ViewModels/LandViewModel.cs
+++ lands/lands/lands/ViewModels/LandViewModel.cs
@@ -4,10 +4,16 @@
     using Lands.ViewModels;
     using System;
     using System.Collections.ObjectModel;
+    using System.Linq;
+
     public class LandViewModel: BaseViewModel
     {
         #region Attrubutes
+        /*Para mostrar una lista en esta Page necesiamos u observable collection*/
         private ObservableCollection<Borders> bordes;
+        //Propiedad para Currensies
+        private ObservableCollection<Currency> currencies;
+
         #endregion
         #region Properties
         public RootObject Land
@@ -20,6 +26,11 @@
             get { return this.bordes; }
             set { SetValue(ref this.bordes, value); }
         }
+        public ObservableCollection<Currency> Currencies
+        {
+            get { return this.currencies; }
+            set { SetValue(ref this.currencies, value); }
+        }
         #endregion
         //Le estamos obeteniendo el objeto recibido Land
         #region Constructor
@@ -27,11 +38,31 @@
         {
             this.Land = land; ///Lista completa del Json
             this.LoadBordes();//Obtener la lista de Bordes
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);///conversion de la lista en un observable conexcion
         }
         #endregion
         #region Methods
         private void LoadBordes()
         {
+            //Creacion de la lista borders
+            //Instancia de una lista vacia 
+            this.Bordes = new ObservableCollection<Borders>();
+            //Barrer la lista
+            foreach (var bordes in this.Land.Borders)
+            {
+                var listaBordes = MainViewModel.GetInstance().landsList.
+                                                Where(l => l.Alpha3Code == bordes).
+                                                FirstOrDefault();
+                //condicional
+                if (listaBordes !=null)
+                {
+                    this.Bordes.Add(new Borders
+                    {
+                        Code =listaBordes.Alpha3Code,
+                        Name =listaBordes.Name
+                    });
+                }
+            }
             
         }
         #endregion
